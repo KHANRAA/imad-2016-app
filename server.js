8039a26f7b83056bec7dadc44b412a3b18795cb6
KHANRAA <khanraakash@gmail.com>
2016-10-10 15:39:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,9 +4,69 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var nameOne={
+    title: 'Name one.Akash',
+    heading:'Name Two',
+    date:'October 10,2016',
+content:`       <p>
+                    My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.
+                </p>
+                <p>
+                    My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.
+                </p>
+                <p>
+                    My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.  My Name is Akash Khanra.
+               </p> '
+    
+    
+};
+
+var creatTemplate(data){
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+
+var htmlTemplate='
+<html>
+<head>
+    <title>
+       ${title}
+    </title>
+    <meta name ="viewport" content="width=device-width initial-scale=1"/>
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+<div class="element">
+<div>
+<a href="/">Home</a>
+</div>
+<div>
+<hr/>
+<h3>
+    ${heading}
+</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+        </div>
+</div>
+</body>
+</html>
+
+
+';
+return htmlTemplate
+}
+    
+
+
 
 app.get('/name-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'name-one.html'));
+  res.send(craatTemplate(nameOnme));
 });
 
 app.get('/name-two', function (req, res) {
